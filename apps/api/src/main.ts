e00b1c195f834a5b2ec8b8872fdab33489a3ecd7
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2025-05-27 19:02:30 -0300

    feat: enhance security and CORS configuration with cookie-parser and helmet integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
 import { config } from './config';
+
 import { GlobalExceptionFilter } from './filters';
-import { HttpGlobalInterceptor } from './interceptors';
+import { GlobalHttpResponseInterceptor } from './interceptors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-  app.useGlobalInterceptors(new HttpGlobalInterceptor());
+  app.use(cookieParser());
+  app.use(helmet());
+  app.enableCors({
+    origin: config().FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.useGlobalInterceptors(new GlobalHttpResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  app.setGlobalPrefix('api');
 
   await app.listen(config().PORT);
 }
