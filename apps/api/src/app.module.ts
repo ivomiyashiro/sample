8736ddf4dda61f88f116d9c2ec2534f1b4b85ca8
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2025-05-27 19:02:12 -0300

    feat: add authentication and security features with JWT, passport, and throttling

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 import { config } from './config';
 import { DatabaseModule } from './database/database.module';
 import { SampleModule } from './modules/sample/sample.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
+    AuthModule,
     SampleModule,
     DatabaseModule,
     ConfigModule.forRoot({
@@ -15,8 +20,19 @@ import { SampleModule } from './modules/sample/sample.module';
       envFilePath: ['.env.local', '.env'],
       validationSchema: null,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
